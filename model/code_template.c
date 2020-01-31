2b73be6e42766481dee207f4450ebc8368f96840
Francesco Paci <francesco.paci@greenwaves-technologies.com>
2020-01-31 15:08:17 +0100

    Now working with new nntool

--- model/code_template.c
+++ model/code_template.c
@@ -34,7 +34,7 @@ void ${gen.project_name}Model(unsigned int L1Memory, unsigned int L2Memory, unsi
     SetUsedFilesNames(0, 2, "CNN_BasicKernels.h", "${gen.project_name}.h");
     SetGeneratedFilesNames("${gen.project_name}Kernels.c", "${gen.project_name}Kernels.h");
 
-    ${gen.memory_device_generator(indent=1,L3RamUserManaged=1,L3FlashUserManaged=0)}
+    ${gen.memory_device_generator(indent=1)}
 
     SetAT_TestFile("AT_${gen.project_name}Test.inc");
 
@@ -46,7 +46,7 @@ ${gen.kernel_generator(indent=1)}
 #ifdef GRAPH
     CreateGraph("${gen.project_name}CNN",
         /* Arguments either passed or globals */
-    ${gen.global_generator(indent=2,forceinputLocation='',forceoutputLocation='AT_MEM_L3_HRAM')},
+    ${gen.global_generator(indent=2)},
         /* Locals, allocated dynamically */
 ${gen.local_generator(indent=2)}
     );
