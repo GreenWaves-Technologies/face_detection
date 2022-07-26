bcef8967fee3881bd3855aaa62087be68b7034aa
PaulLuperini <paul.luperini@gmail.com>
2022-07-26 16:02:09 +0200

    [MALLOC] fix incorrect `pi_l1_malloc` calls

--- main.c
+++ main.c
@@ -552,8 +552,8 @@ int face_detection()
         face_detectionCNN_Destruct();
 
         //SSD Allocations
-        SSDKernels_L1_Memory = pi_l1_malloc(_SSDKernels_L1_Memory_SIZE);
-        SSDKernels_L2_Memory = pi_l2_malloc(_SSDKernels_L2_Memory_SIZE);
+        SSDKernels_L1_Memory = pi_l1_malloc(&cluster_dev, _SSDKernels_L1_Memory_SIZE);
+        SSDKernels_L2_Memory = pi_l2_malloc(&cluster_dev, _SSDKernels_L2_Memory_SIZE);
 
         if(face_detection_L1_Memory==NULL || face_detection_L2_Memory==NULL)
         {
@@ -566,8 +566,8 @@ int face_detection()
         pi_cluster_task_stacks(task, NULL, CLUSTER_SLAVE_STACK_SIZE);
         pi_cluster_send_task_to_cl(&cluster_dev, task);
 
-        pi_l1_free(SSDKernels_L1_Memory,_SSDKernels_L1_Memory_SIZE);
-        pi_l2_free(SSDKernels_L2_Memory,_SSDKernels_L2_Memory_SIZE);
+        pi_l1_free(&cluster_dev, SSDKernels_L1_Memory,_SSDKernels_L1_Memory_SIZE);
+        pi_l2_free(&cluster_dev, SSDKernels_L2_Memory,_SSDKernels_L2_Memory_SIZE);
 
 
         for(int y=0;y<120;y++){
